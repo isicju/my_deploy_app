9257a71f7331639a56128679d063aafd43376d5f
icju <isicju@gmail.com>
2026-09-01 02:22:59 +0300

    kek

--- src/test/java/com/kotojava/IntegrationTests.java
+++ src/test/java/com/kotojava/IntegrationTests.java
@@ -3,9 +3,7 @@ package com.kotojava;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@SpringBootTest
 public class IntegrationTests {
 
     @Test
--- src/test/java/com/kotojava/UnitTests.java
+++ src/test/java/com/kotojava/UnitTests.java
@@ -3,9 +3,6 @@ package com.kotojava;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
 public class UnitTests {
 
     @Test
