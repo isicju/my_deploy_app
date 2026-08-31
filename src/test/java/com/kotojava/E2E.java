061f5e2c52aae836809f1a5788155a64eb512aa5
icju <isicju@gmail.com>
2026-09-01 02:22:26 +0300

    kek

--- src/test/java/com/kotojava/E2E.java
+++ src/test/java/com/kotojava/E2E.java
@@ -11,12 +11,12 @@ public class E2E {
     @Test
     public void quickTest1() throws InterruptedException {
         System.out.println("printing E2E test 1");
-        Thread.sleep(2000);
+        Thread.sleep(10000);
     }
 
     @Test
     public void quickTest2() throws InterruptedException {
-        Thread.sleep(2000);
+        Thread.sleep(10000);
     }
 
 }
