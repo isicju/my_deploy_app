f310f2ef9b0de09f9445695e513c14c9f573982d
icju <isicju@gmail.com>
2026-09-01 02:12:12 +0300

    kek

---
+++ src/test/java/com/kotojava/Fake.java
@@ -0,0 +1,14 @@
+package com.kotojava;
+
+
+import org.junit.jupiter.api.Test;
+
+public class Fake {
+
+
+    @Test
+    public void forceRightBuild(){
+        System.out.println("I forced gradle to compile everything properly  including test sources");
+    }
+
+}
